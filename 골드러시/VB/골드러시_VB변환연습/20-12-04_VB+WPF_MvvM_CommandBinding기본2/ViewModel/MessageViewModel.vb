a022f2c6aa41092ab0ec5757cf33a5ea4ea676fb
Seong-gon Lee <gongom@live.com>
2020-12-04 11:07:15 +0900

    설명내용 추가함

--- 골드러시/VB/골드러시_VB변환연습/20-12-04_VB+WPF_MvvM_CommandBinding기본2/ViewModel/MessageViewModel.vb
+++ 골드러시/VB/골드러시_VB변환연습/20-12-04_VB+WPF_MvvM_CommandBinding기본2/ViewModel/MessageViewModel.vb
@@ -1,4 +1,8 @@
-﻿
+﻿'현재까지 TextBox에 속성하나, Button에 명령하나를 사용하였다.
+'    여기에서 TextBox의 속성을 이용하지않고 Button 클릭시
+'    명령에 파라미터로 현재 입력한 TextBox의 텍스트를 대입하는 
+'    방법을 알아보자.
+'    우선은 TextBox에 바인딩되었던 MessageText 속성을 주석처리하고, DisplayMessage에 파라미터로 String message 를 대입한다.
 
 Namespace ViewModel
     Public Class MessageViewModel
