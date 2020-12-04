55e872e7e015d66d0ed2faaf97794d9bfeb1f821
Seong-gon Lee <gongom@live.com>
2020-12-04 14:14:55 +0900

    Command3 예제 작성됨

---
+++ 골드러시/VB/골드러시_VB변환연습/20-12-04_VB+WPF_MvvM_CommandsDemo3/ViewModel/Command/DelegateCommand.vb
@@ -0,0 +1,58 @@
+﻿Public Class DelegateCommand
+    Implements ICommand
+
+    '명령에 필요한 실제 수행 동작(Action)이 들어갈 함수인
+    '_execute와 조건(Predicate) 체크에 사용된 _canExecute를 선언한다.
+    '<obejct> 는 나중에 사용할 파라미터의 형태이다.
+
+    ReadOnly mExecute As Action(Of Object)
+    ReadOnly mCanExecute As Predicate(Of Object)
+
+    '다음 생성자는 수행 동작(Action)과 조건(Predicate)에 사용될 파라미터를 받아서
+    '현재 클래스의 _execute와 _canExecute에 대입해 주는 부분이고,
+    '앞쪽의 execute는 반드시 입력해야 하는 파라미터로 null이되면 오류를 발생하는 조건을 넣었다. 
+    Public Sub New(execute As Action(Of Object), canExecute As Predicate(Of Object))
+
+        If execute Is Nothing Then
+            Throw New NullReferenceException("Execute cannot Nothing")
+        End If
+
+        mExecute = execute
+        mCanExecute = canExecute
+
+    End Sub
+
+    '다음 생성자는
+    '앞쪽의 execute 동작만 입력 받으면 (execute, null)
+    '파라미터를 ①의 생성자로 전달하는 구문이다.
+    Public Sub New(execute As Action(Of Object))
+        Me.New(execute, Nothing)
+
+    End Sub
+
+    'Public Event CanExecuteChanged As EventHandler Implements ICommand.CanExecuteChanged
+
+    'CanExecuteChanged 이벤트 핸들러는 화면 상에서 변경이 발생하면 인식을 못하므로 아래와 같이,
+    '변경이 발생하면 CommandManager에 이벤트를 추가(add)하거나 제거(remove)해 주는 과정이 필요하다.
+    Public Custom Event CanExecuteChanged As EventHandler Implements ICommand.CanExecuteChanged
+        AddHandler(ByVal value As EventHandler)
+            AddHandler CommandManager.RequerySuggested, value
+        End AddHandler
+        RemoveHandler(ByVal value As EventHandler)
+            RemoveHandler CommandManager.RequerySuggested, value
+        End RemoveHandler
+        RaiseEvent()
+        End RaiseEvent
+    End Event
+
+    'Execute 함수는 넘어온 함수를 파라미터와 함께 그대로 수행하는 내용이다.
+    Public Sub Execute(parameter As Object) Implements ICommand.Execute
+        mExecute.Invoke(parameter)
+    End Sub
+
+    'CanExecute 함수는 _canExecute가 넘어오지 않으면
+    '별다른 조건 체크 없이 항상 true를 반환하고, 넘어왔다면 _canExecute(parameter) 수행을 반환한다.
+    Public Function CanExecute(parameter As Object) As Boolean Implements ICommand.CanExecute
+        Return If(mCanExecute Is Nothing, True, mCanExecute(parameter))
+    End Function
+End Class
---
+++ 골드러시/VB/골드러시_VB변환연습/20-12-04_VB+WPF_MvvM_CommandsDemo3/ViewModel/MainViewModel.vb
@@ -0,0 +1,57 @@
+﻿Imports System.Collections.ObjectModel
+
+Namespace ViewModel
+    Public Class MainViewModel
+
+        '실제 구현될 UI 구성이 콤보 박스와 버튼 2개이므로, 
+        '콤보박스용 MyMessages 속성하나와 만들어 두었던 DelegateCommand 형태를 갖는 
+        'MessageBoxCommand, ConsoleCommand 속성을 선언한다.
+        'ObservableCollection은 WPF의 리스트(List) 속성과 완벽하게 호환되는 컬렉션이다.
+
+        Public Property MyMessages As ObservableCollection(Of String)
+        Public Property MessageBoxCommand As DelegateCommand
+        Public Property ConsoleCommand As DelegateCommand
+
+
+        Public Sub New()
+
+            'String 형태의 리스트 컬렉션을 갖는 MyMessages를 초기화하면서 값을 대입한다.
+            MyMessages = New ObservableCollection(Of String)() From {
+            "Hi", "Hello, World", "Message Box", "Console"
+            }
+
+            MessageBoxCommand = New DelegateCommand(AddressOf DisplayInMessageBox, AddressOf MessageBoxCanUse)
+            ConsoleCommand = New DelegateCommand(AddressOf DisplayInConsole, AddressOf ConsoleCanUse)
+
+        End Sub
+
+        'UI상의 첫 번째 버튼인 "Message Box"를 클릭 했을 때 수행할 DisplayInMessageBox 함수와
+        '두 번째 버튼인 "Console Log"를 클릭 했을 때 수행할 DisplayInConsole 함수를 추가한다.
+        '내용은 각 파라미터로 넘어온 내용을 MessageBox와 Console로 보여주는 내용이다.(범용성을 위해 파라미터가 Object 형태이므로 string으로 형변환이 필요하다.)
+        Public Sub DisplayInMessageBox(message As Object)
+            MessageBox.Show(CStr(message))
+        End Sub
+
+        Public Sub DisplayInConsole(message As Object)
+            Console.WriteLine(CStr(message))
+        End Sub
+
+        '두 버튼에 대한 조건도 각각 추가하는데,
+        '콤보 박스 선택 내용이 "콘솔"이면 첫 번째 버튼은 사용 불가로 되고
+        '"메시지 박스"이면 두 번째 버튼이 사용 불가로 되는 조건이다.
+        Public Function MessageBoxCanUse(message As Object) As Boolean
+            If CStr(message) = "Console" Then
+                Return False
+            End If
+            Return True
+        End Function
+
+        Public Function ConsoleCanUse(message As Object) As Boolean
+            If CStr(message) = "Message Box" Then
+                Return False
+            End If
+            Return True
+        End Function
+    End Class
+End Namespace
+
