64cd144d306c8f509aa447e0b9caf4829389f537
Seong-gon Lee <gongom@live.com>
2021-01-05 18:16:50 +0900

    BindConversion 프로젝트 추가함

---
+++ 마이크로소프트/WPF샘플변환/WPF샘플/BindConversion/MyConverter.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Globalization
+
+Public Class MyConverter
+    Implements IValueConverter
+
+    Public Function ConvertBack(value As Object, targetType As Type, parameter As Object, culture As CultureInfo) As Object Implements IValueConverter.ConvertBack
+        Return Nothing
+    End Function
+
+    Public Function IValueConverter_Convert(o As Object, type As Type, parameter As Object, culture As CultureInfo) As Object Implements IValueConverter.Convert
+        Dim _date = CType(o, DateTime)
+        Select Case type.Name
+            Case "String"
+                Return _date.ToString("F", culture)
+            Case "Brush"
+                Return Brushes.Red
+            Case Else
+                Return o
+        End Select
+    End Function
+End Class
---
+++ 마이크로소프트/WPF샘플변환/WPF샘플/BindConversion/MyData.vb
@@ -0,0 +1,26 @@
+﻿Imports System.ComponentModel
+
+Public Class MyData
+    Implements INotifyPropertyChanged
+
+    Private _thedate As DateTime
+    Public Sub New()
+        _thedate = DateTime.Now
+    End Sub
+
+    Public Property TheDate As DateTime
+        Get
+            Return _thedate
+        End Get
+        Set(value As DateTime)
+            _thedate = value
+            OnPropertyChanged("TheDate")
+        End Set
+    End Property
+
+    Protected Sub OnPropertyChanged(propertyName As String)
+        RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs(propertyName))
+    End Sub
+
+    Public Event PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
+End Class
